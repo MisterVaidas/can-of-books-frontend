38b85fb851267b0a9fa1e9684cdc0e122fb81c67
Joe Venton <joe.venton@gmail.com>
2023-08-01 14:25:56 +0100

    created book carousel render

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -7,21 +9,33 @@ class BestBooks extends React.Component {
       books: []
     }
   }
-
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount() {
+    fetch("http://localhost:3001/books")
+    .then(response => response.json())
+    .then(data => this.setState({books:data}))
+    .catch(error => console.error("Error", error))
+  }
 
   render() {
-
-    /* TODO: render all the books in a Carousel */
+/* TODO: render all the books in a Carousel */
+    
 
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+      
+        {this.state.books.length > 0 ?  (
+          <Carousel>
+          {this.state.books.map((book, index) => (
+         <div key={index}>
+          <h2>{book.title}</h2>
+          <p>{book.author}</p>
+         </div>
+            ))}
+            </Carousel>
         ) : (
-          <h3>No Books Found :(</h3>
+          <h3>"No Books Found :</h3>
         )}
       </>
     )
