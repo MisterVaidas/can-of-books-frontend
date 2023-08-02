6f291016f47a24f030f20260e3cd7e816506a890
Joe Venton <joe.venton@gmail.com>
2023-08-02 11:43:44 +0100

    added forms function

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -7,10 +7,41 @@ class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
-    }
+      books: [],
+      newBook: {
+        title: "",
+        author: "",
+        description: "",
+        status: [],
+        coverImageUrl: "",
+      },
+    };
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  handleChange(event) {
+    this.setState({newBook: {...this.state.newBook, [event.target.name]: event.target.value}});
+  } 
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch("http://localhost:3001/books", {
+      method: "both",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state.newBook),
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.setState({books: [...this.state.books, data]});
+      this.setState({newBook: {title: "", author: "", description: "", status: [], coverImageUrl: "" }});
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  }
   componentDidMount() {
     fetch("http://localhost:3001/books")
     .then(response => response.json())
@@ -23,7 +54,26 @@ class BestBooks extends React.Component {
     return (
       <div className="best-books-container">
         <h2 className="title">My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Title:
+            <input type ="text" name="title" value={this.state.newBook.title} onChange={this.handleChange} />
+          </label>
+          <label>
+            Author:
+            <input type="text" name="author" value={this.state.newBook.author} onChange={this.handleChange} />
+          </label>
+          <label>
+            Description:
+          <input type="text" name="description" value={this.state.newBook.description} onChange={this.handleChange} />
+          </label>
+          <label>
+            Cover Image Link:
+            <input type="text" name="coverImageUrl" value={this.state.newBook.coverImageUrl} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+          </form>
+          
         {this.state.books.length > 0 ? (
           <Carousel className="book-carousel">
             {this.state.books.map((book, index) => (
